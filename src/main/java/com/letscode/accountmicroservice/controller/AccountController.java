86f0cd004c46e81f78c87d0322e3889e4c8a764c
Rafes <rafadvcoliveira@gmail.com>
2022-04-22 18:54:37 -0300

    Create AccountController

--- src/main/java/com/letscode/accountmicroservice/controller/AccountController.java
+++ src/main/java/com/letscode/accountmicroservice/controller/AccountController.java
@@ -1,4 +1,23 @@
 package com.letscode.accountmicroservice.controller;
 
+import com.letscode.accountmicroservice.dto.AccountRequest;
+import com.letscode.accountmicroservice.dto.AccountResponse;
+import com.letscode.accountmicroservice.services.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/account")
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping("/")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public AccountResponse create(@RequestBody AccountRequest accountRequest) {
+
+        return accountService.create(accountRequest);
+    }
 }
